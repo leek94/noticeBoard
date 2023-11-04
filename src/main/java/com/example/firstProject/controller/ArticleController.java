3f6f1c2f0643a48c604e91acce11f58ead967134
leek94 <khwan929@naver.com>
2023-11-04 10:42:04 +0900

    refator: Article 롬복 리팩토링

--- src/main/java/com/example/firstProject/controller/ArticleController.java
+++ src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstProject.controller;
 import com.example.firstProject.dto.ArticleForm;
 import com.example.firstProject.entity.Article;
 import com.example.firstProject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
 
     @Autowired
@@ -20,13 +22,16 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) { //form 정보 매개변수로 받아옴
-        System.out.println(form.toString());
+        log.info(form.toString());
+//        System.out.println(form.toString());
         //1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+//        System.out.println(article.toString());
         //2. 리포지토리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+//        System.out.println(saved.toString());
         return "";
     }
 
--- src/main/java/com/example/firstProject/entity/Article.java
+++ src/main/java/com/example/firstProject/entity/Article.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@ToString
 public class Article {
 
     @Id // 엔티티 대푯값 지정
@@ -21,18 +25,4 @@ public class Article {
     @Column
     private String content;
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 }
