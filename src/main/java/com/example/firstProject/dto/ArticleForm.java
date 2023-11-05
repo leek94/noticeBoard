668a1048c2cd2f73fa623799442bc290c264efde
leek94 <khwan929@naver.com>
2023-11-05 23:09:50 +0900

    feat: 수정 DB 저장 완료

--- src/main/java/com/example/firstProject/dto/ArticleForm.java
+++ src/main/java/com/example/firstProject/dto/ArticleForm.java
@@ -7,10 +7,11 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 public class ArticleForm {
+    private Long id;
     private String title;
     private String content;
 
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
