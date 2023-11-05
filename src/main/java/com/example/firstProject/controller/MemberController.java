fea3d8a0efe9194648ea847c41c79ed207cb67ed
leek94 <khwan929@naver.com>
2023-11-05 16:48:34 +0900

    feat: member 조회 생성

--- src/main/java/com/example/firstProject/controller/MemberController.java
+++ src/main/java/com/example/firstProject/controller/MemberController.java
@@ -6,7 +6,9 @@ import com.example.firstProject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -34,6 +36,20 @@ public class MemberController {
         log.info(saved.toString());
 //        System.out.println(saved.toString());
         return "";
+    }
 
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        Iterable<Member> memberEntityList = memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+        return "";
     }
 }
