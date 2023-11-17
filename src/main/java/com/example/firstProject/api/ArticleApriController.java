8541c699b8dea21ee4953c02fe9be74f0438c940
leek94 <khwan929@naver.com>
2023-11-17 15:56:07 +0900

    feat: REST API delete 생성

--- src/main/java/com/example/firstProject/api/ArticleApriController.java
+++ src/main/java/com/example/firstProject/api/ArticleApriController.java
@@ -49,10 +49,21 @@ public class ArticleApriController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         // 4. 업데이트 및 정상 읍당(200) 하기
-
-
-        Article updated = articleRepository.save(article);
+        target.patch(article);
+        Article updated = articleRepository.save(target);
         return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
     // DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        // 1. 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+        // 2. 잘못된 요청 처리하기
+        if (target == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        // 3. 대상 삭제하기
+        articleRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).body(null); // .body(null) 이랑 .build() 랑 같은값
+    }
 }
